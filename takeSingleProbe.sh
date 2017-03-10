e3ed341a90bd28c1c69ae31ff1f0b511ad33ddce
mirkoebert <mirko.ebert@gmail.com>
2017-03-10 22:45:23 +0100

    script is now working

--- takeSingleProbe.sh
+++ takeSingleProbe.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
 LANG=en_US.UTF-8
-logfile=test_complex_curl_imageserver_${user}.log
+logfile=result.csv
+
 useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 curl/performancetest"
+curlops1="-k --connect-timeout 30 --limit-rate 752k --compressed -w @curl-format.txt -o /dev/null -s"
 
-curlops1=" -k --connect-timeout 30 --limit-rate 752k --compressed -w @curl-format.txt -o /dev/null -s "
+url=`rl -c 1 url.txt`
+c="$curlops1 $url"
 
 
-while read line; do
-    c="$curlops1 $line"
-    cx="$cx $c"
-done < ${user}.txt
+result=`curl --user-agent "$useragent "  $c`
 
-d=`date`
-result=`curl --user-agent "$useragent "  $cx`
-
-count=1
-IFS='!' read -ra ADDR <<< "$result"
-for i in "${ADDR[@]}"; do
-    echo "$date; $i;  T${count}$2" >> $logfile
-    ((count = count + 1))
-done
+echo "$(date),$(hostname);${result}" >> $logfile
 
 
 
